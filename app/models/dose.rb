cc26dff061ddfa7742ed4e63e8d8ee3a4e1db0bd
Tomas Trzicky <tomas.trzicky1@seznam.cz>
2020-11-05 15:41:18 +0000

    associations validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
+  #association
   belongs_to :cocktail
   belongs_to :ingredient
+  #validation
+  validates :description, :ingredient, :cocktail, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
